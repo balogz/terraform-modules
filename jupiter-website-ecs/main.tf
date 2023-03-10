38362644e409c083f8a2357ce19a870acdfb70fd
balogz <balorad07@yahoo.com>
2023-03-10 16:39:58 +0000

    create alb

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -56,4 +56,13 @@ module "acm" {
   
   }
 
-
+# create alb
+module "alb" {
+  source = "../modules/alb"
+  project_name=module.vpc.project_name
+  alb_security_group_id=module.security_group.alb_security_group_id
+  public_subnet_az1_id=module.vpc.public_subnet_az1_cidr
+  public_subnet_az2_id=module.vpc.public_subnet_az2_cidr
+  vpc_id=module.vpc.vpc_id
+  certificate_arn=module.acm.certificate_arn
+  }
